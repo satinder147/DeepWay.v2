b37613764804a44060f54f5b33911f3df31be868
satinder147 <science.reactor85@gmail.com>
2020-02-14 22:26:42 +0530

    using tensorrt model for better fps in blind runner.py

--- blindrunner.py
+++ blindrunner.py
@@ -1,38 +1,54 @@
-from modelArch.unet import Unet
-#from unet.unet_model import UNet
+'''
+Things to complete by tonight
+1) make a proper blinderRunner.py
+2) make pytorch to trt converters
+3) runn the system with face detection and stop sign detection tonight
+4) run python file on startup
+5) start updating the readme add to TODO convert unet to tensorrt
+6) look for ways to improve lane prediction
+7) if lane prediction happens, use exponential avaerges for dealing with classification errors
+8) make a swap file
+'''
+
+
 import torch
-import torchvision
+import time
 from torchvision import transforms
 import cv2
 from PIL import Image
 import numpy as np
-from torchsummary import summary
+#from torchsummary import summary
 from modelArch.cnn import Cnn
-net=Cnn()
-net.load_state_dict(torch.load("lane.pth"))
+from modelArch.unet import Unet
+from torch2trt import TRTModule
+#net=Unet(3,1)
+
+
+
+cnn=TRTModule()
+cnn.load_state_dict(torch.load("lane_trt.pth"))
 device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("using ",device)
-net.to(device)
-summary(net,input_size=(3,64,64))
 trans=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
-
 cap=cv2.VideoCapture("dataSet/videos/first.mp4")
 dic={0:"left",1:"center",2:"right"}
+
+
 while True:
     with torch.no_grad():
+        start=time.time()
         img=cap.read()[1]
-        #img=cv2.imread("/home/satinder/Desktop/deepWay/DeepWay.v2/dataSet/segmentation1/img/85.jpg",1)
         show=img.copy()
-        show=cv2.resize(show,(640,480))
-        img=cv2.resize(img,(64,64))
+        #show=cv2.resize(show,(640,480))
+        img=cv2.resize(img,(256,256))
         img=trans(img).unsqueeze(0)
-        #img=img.type(torch.FloatTensor)
         img=img.to(device)
         label=net(img)
         lane=torch.argmax(label[0].cpu().detach()).numpy()
-        #print(dic[lane.item()])
-        cv2.putText(show,dic[lane.item()],(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2)
-        cv2.imshow("img",show)
+        end=time.time()
+        fps=str(int(1/(end-start))_
+        cv2.putText(show,dic[lane.item()]+"  fps: "+fps,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+        #cv2.imshow("img",show)
         cv2.waitKey(1)
 
 
@@ -51,4 +67,5 @@ while True:
         cv2.imshow("img",img)
         cv2.waitKey(1)
         #break
-'''
+        '''
+
