da71b7cda87baa520eed9fde3f27bdbe893ad705
satinder147 <science.reactor85@gmail.com>
2020-02-17 02:22:39 +0530

    object detection(person) based on Hao' repository

--- pedestrian.py
+++ pedestrian.py
@@ -11,15 +11,18 @@ class detector:
     def __init__(self):
 
         self.class_names = [name.strip() for name in open(label_path).readlines()]
-        num_classes = len(class_names)
-        self.net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
+        self.num_classes = len(self.class_names)
+        self.net = create_mobilenetv2_ssd_lite(len(self.class_names), is_test=True)
         self.net.load(model_path)
-        self.predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200,device=torch.device("cuda:0"))
+        self.predictor = create_mobilenetv2_ssd_lite_predictor(self.net, candidate_size=200,device=torch.device("cuda:0"))
     
     def return_boxes(self,img):
         img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        boxes,labels,prob=self.predictor(img,10,0.4)
+        boxes,labels,prob=self.predictor.predict(img,10,0.4)
         return boxes,labels
+    
+    def label(self,label):
+        return self.class_names[label]
         
 
 
